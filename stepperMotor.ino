fd8fcb5d7157f1ef938b7f3f48a88bf3211c61dc
Žan Bulič <96945150+zanbulic@users.noreply.github.com>
2022-04-19 20:41:20 +0200

    Added servo motor

--- stepperMotor.ino
+++ stepperMotor.ino
@@ -1,5 +1,6 @@
 #include <Stepper.h>
- 
+#include <Servo.h>
+
 #define STEPS 32
 
 #define IN1  11
@@ -9,15 +10,22 @@
  
 Stepper stepper(STEPS, IN4, IN2, IN3, IN1);
 
+
 #define joystick_x A0
+#define joystick_y A1
+
+Servo servo;
+
+int servoPosition = 0;
 
 void setup() {
-
+  servo.attach(3);
 }
  
 void loop() {
   int valx = analogRead(joystick_x);
- 
+  int valy = analogRead(joystick_y);
+
   if((valx > 500) && (valx < 523)) {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
@@ -26,22 +34,25 @@ void loop() {
   }
  
   else {
-    while (valx >= 523) {
+    if (valx >= 523) {
       int speed_ = map(valx, 523, 1023, 1, 400);
       stepper.setSpeed(speed_);
  
       stepper.step(1);
- 
-      valx = analogRead(joystick_x);
     }
  
-    while (valx <= 500) {
+    if (valx <= 500) {
       int speed_ = map(valx, 500, 0, 1, 400);
       stepper.setSpeed(speed_);
  
       stepper.step(-1);
- 
-      valx = analogRead(joystick_x);
     }
+  }
+  if (valy >= 523 && servoPosition <= 180) {
+    servoPosition++;
+    servo.write(servoPosition);
+  } else if (valy <= 500 && servoPosition >=0) {
+    servoPosition--;
+    servo.write(servoPosition);
   }
 }
